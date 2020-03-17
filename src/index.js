c8eec09d3663e17ce9bcac4e75d756f360e8045f
Adam <33174392+Adorek@users.noreply.github.com>
2020-03-17 19:16:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  val = bracketsConfig.map(e => e.join("")).join("?").replace(/[{}()|[\]]/g, '\\$&').replace(/\?/g, '|');
+  while (new RegExp(val, "g").test(str)) {
+    str = str.replace(new RegExp(val, "g"), "")     
+  }
+  return !str.length;
 }
